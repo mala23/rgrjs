1ca3ca1fcb3dc687a335f48ca07608ad5986209d
Colin Nico Schmid <mail@mala23.me>
2018-02-20 14:44:07 +0100

    push to debug with bro

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Hello from "./components/Hello.js"
+
+ReactDOM.render(<p>hello</p>, document.getElementById('react'))
--- public/bundle.js
+++ public/bundle.js
@@ -67,8 +67,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log('Hello from JS')
-
+throw new Error("Module build failed: SyntaxError: Unexpected token (6:16)\n\n\u001b[0m \u001b[90m 4 | \u001b[39m\u001b[36mimport\u001b[39m \u001b[33mHello\u001b[39m from \u001b[32m\"./components/Hello.js\"\u001b[39m\n \u001b[90m 5 | \u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 6 | \u001b[39m\u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender(\u001b[33m<\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39mhello\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'react'\u001b[39m))\n \u001b[90m   | \u001b[39m                \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 7 | \u001b[39m\u001b[0m\n");
 
 /***/ })
 /******/ ]);
